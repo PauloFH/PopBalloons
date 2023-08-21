f5d5c29c54b5025a8c7ee0c043c879c386383d0d
Paulo Holanda <paulorfholanda@gmail.com>
2023-08-21 19:30:36 -0300

    Miado ocasionamente

--- PopBalloons/Cat.cpp
+++ PopBalloons/Cat.cpp
@@ -1,19 +1,36 @@
 #include "Cat.h"
 
 Cat::Cat() {
+    frames = 0;
     tileset = new TileSet("Resources/cat.png", 48, 48, 15, 15);
     animation = new Animation(tileset, 0.5f, true);
-
+    audio = new Audio();
+    audio->Add(100, "Resources/cat.wav");
     MoveTo(900, 505);
+
 }
 
 Cat::~Cat() {
     delete tileset;
     delete animation;
+    delete audio;
 }
 
 void Cat::Update() {
+
+    frames++;
+
+    if (frames >= ctFrames) {
+        controlador = true;
+    }
+
+    if (controlador) {
+        frames = 0;
+        audio->Play(100);
+        controlador = false;
+    }
     animation->NextFrame();
+
 }
 
 void Cat::OnCollision(Object* obj) {
--- PopBalloons/Cat.h
+++ PopBalloons/Cat.h
@@ -1,12 +1,16 @@
 #include "Object.h"
 #include "TileSet.h"
 #include "Animation.h"
+#include "Audio.h"
 
 class Cat : public Object {
 private:
-	TileSet* tileset;
+	TileSet  * tileset;
 	Animation* animation;
-
+	Audio    * audio;
+	int frames = 0;
+	int ctFrames = 1600;
+	bool controlador = false;
 public:
 	Cat();
 	~Cat();
